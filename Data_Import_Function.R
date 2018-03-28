b00702e429bed8b7b8828161e9bf4b62033cdf39
Emma <emhauser@ku.edu>
2018-03-28 17:22:28 -0500

    adding data import function

---
+++ Data_Import_Function.R
@@ -0,0 +1,4 @@
+##DataImportFunction##
+#This is a function to scrape a datatable from a pdf and append the data to an existing .csv file. 
+#The two datasets will be merged according to common columns of 'land use' and 'site'
+#The function will return a single dataframe with columns from both the pdf and the .csv
