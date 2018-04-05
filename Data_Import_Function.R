028fdb082b841f17cb9afc43a2ff8d0dc2d61095
Emma <emhauser@ku.edu>
2018-04-05 15:47:39 -0500

    working on function writing

--- Data_Import_Function.R
+++ Data_Import_Function.R
@@ -6,12 +6,33 @@
 #Args
   #csv : calls the names of the csv files to import. May be a single csv or a vector of csv files
 
+help("list.files")
 data_import <- function(csv)
 {
- #for(set in CSVvector)
   {
-  #soilDatFrame <- read.csv(csv)
-  #name(soilDatFrame[set]) <- "set" #here give each dataframe its own name
+    filenames <- list.files(pattern=".*csv")
+filenames
+    ##Create list of data frame names without the ".csv" part 
+    names <-substr(filenames,1,3)
+    names
+
+    ###Load all files
+    for(i in names)
+      {
+          filepath <- file.path(paste(i,".csv",sep=""))
+          assign(i, read.delim(filepath,
+                       colClasses=c("character","factor",rep("numeric",4)),
+                       sep = "\t"))
+          for(i in names)
+          {
+            oname = paste(i, sep="")
+            assign(oname, read.csv(paste(oname, ".txt", sep="")))
+          }
+      }
+  }
+}
+  soilDatFrame <- read.csv(csv)
+  
   #return(SoilDatFrame[set])
   }
 }
