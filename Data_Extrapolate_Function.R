f0d13e2a1dd5f341ebd69ade9d03b02ff80edf2d
Emma <emhauser@ku.edu>
2018-04-07 09:50:26 -0500

    Working on data_fill function

--- Data_Extrapolate_Function.R
+++ Data_Extrapolate_Function.R
@@ -5,56 +5,68 @@
 #The function will return a single dataframe with all the response variables. 
 
 #Args:
-  #soilDatSets: This is a list containing names of the dataframes imported in the data_import function
+  #soilDatSet: This is a dataframe from which the user is extrapolating datapoints via linear interpolation
 
-Data_Fill <- function(soilDatSets=SoilDatList)
-{
+#Initial pseudocode:
+#Data_Fill <- function(soilDatSets=SoilDatList)
+#{
   #soilDat <- Make and empty dataframe into which columns of data may be added
-  for(set in soilDatSets)
-  {
+  #for(set in soilDatSets)
+  #{
     #plot(responseVar ~ soil depth)
     #make as smooth curve over the points
     #extrapolate a value at each 1 cm depth up to 2m
     #put values in a vector
     #soilDat[set] <- new vector becomes a dataframe column
-  }
+  #}
   #return(soilDat)
+#}
+
+
+#Function that produces a plot and a set of response variable points generated using linear interpolation between sampled datapoints, as well as a plot of extrapolated and measured points.
+data_fill <- function (soilDatSet)
+{
+  x <-soilDatSet[,2]
+  y <- soilDatSet[,1]
+df1 <- data.frame(x,y)
+dfOrdered <- df1
+plot(x=dfOrdered$x,y=dfOrdered$y, main = "approx(.) and approxfun(.)", ylim = c(180,0))
+demo <- (approx(y=dfOrdered$x, x=dfOrdered$y, xout=seq(4,175)))
+plot(demo$x~demo$y, ylim = c(180,0))
+return(demo$y)
 }
 
-
-PFrac
-Roots
-
-OrderedDat <- PFrac[order(PFrac$Pi.Fract),]
-xDat <- OrderedDat$Pi.Fract
-yDat <- OrderedDat$Depth
-
-plot(x=xDat, y=yDat, ylim = c(180,0))
-points(approx(x=xDat, y=yDat, n=200), col = 2, pch = "*")
-demo <- (approx(x=xDat, y=yDat, xout=seq(1,200)))
-demo
-
-plot(demo$x~demo$y, ylim = c(180,0))
-str(demo)
-demo
-help("approx")
-install.packages("zoopackage")
-
-points(approx(x, y, method = "constant"), col = 4, pch = "*")
-
-f <- approxfun(x, y)
-curve <- curve(f(x), 0, 10, col = "green")
-points(x, y)
-is.function(fc <- approxfun(x, y, method = "const")) # TRUE
-curve(fc(x), 0, 10, col = "darkblue", add = TRUE)
+#Then Produce a dataframe with each extrapolated response variable
+#This could be more modular. I need to make my dataframes into a vector. However, I'm having trouble getting my function to run when I do this.
+#To make a vector, I think I should do this:
+# soilDataSets <- as.vector(PiFra, PoFra, EOC.c, AP.cs, AP.EO)
+  
+SoilSummDat <- as.data.frame(4:175)
+colnames(SoilSummDat) <- "Depth"
+SoilSummDat$Pi <- Dat_ext(PiFra)
+SoilSummDat$Po <- Dat_ext(PoFra)
+SoilSummDat$EOC <- Dat_ext(EOC.c)
+SoilSummDat$AP <- Dat_ext(AP.cs)
+SoilSummDat$APEOC <- Dat_ext(AP.EO)
 
 
 
 
-####Other attempt
+
+
+
+
+
+
+#FIRST ATTEMPT#
 
 SoilSummDat <- as.data.frame(4:175)
-x <- PFrac$Po.Fract
+colnames(SoilSummDat) <- "Depth"
+DepthVars <- c("Depth","Depth", "mean_depth_.cm. ","mean_depth_.cm.", "mean_depth_.cm. ")
+RespVars <- c("Po.Fract", "Pi.Fract", "EOC.mgC_g.1",  "AP.mgC_g.1", "AP.EOC")
+SoilDatList
+
+x <-PFrac$Po.Fract
 y <- PFrac$Depth
 df1 <- data.frame(x,y)
 dfOrdered <- df1
@@ -62,6 +74,6 @@ plot(x=dfOrdered$x,y=dfOrdered$y, main = "approx(.) and approxfun(.)", ylim = c(
 demo <- (approx(y=dfOrdered$x, x=dfOrdered$y, xout=seq(4,175)))
 plot(demo$x~demo$y, ylim = c(180,0))
 SoilSummDat <- cbind(SoilSummDat, demo$y)
-
-
+colnames(SoilSummDat)[2] <- "Po"
+head(SoilSummDat)
 
